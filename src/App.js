2a8dc3e436f6d890620d8486bf15001831b8c291
Peter McClelland <peterwmcclelland@gmail.com>
2023-09-21 18:58:24 -0500

    split tip feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     const [bill, setBill] = useState('');
     const [tip, setTip] = useState(0);
     const [total, setTotal] = useState(0);
+    const [splitCount, setSplitCount] = useState(1);
 
     const calculateTip = (percentage) => {
         let calculatedTip = (bill * (percentage / 100)).toFixed(2);
@@ -13,18 +14,18 @@ function App() {
         setTotal(calculatedTotal);
     };
 
+    const perPersonAmount = (total / splitCount).toFixed(2);
+
     return (
         <div className="app">
             <h1>Here's A Tip</h1>
             <div className="input-group">
                 <label>Bill Amount:</label>
                 <input
-                    
                     type="number"
                     value={bill}
                     onChange={(e) => setBill(e.target.value)}
                 />
-                
             </div>
             <div className="input-group">
                 <label>Select Tip Percentage:</label>
@@ -34,14 +35,21 @@ function App() {
                     <button onClick={() => calculateTip(20)}>20%</button>
                 </div>
             </div>
+            <div className="input-group">
+                <label>Split Bill:</label>
+                <select value={splitCount} onChange={(e) => setSplitCount(e.target.value)}>
+                    {Array.from({length: 10}, (_, i) => (
+                        <option key={i+1} value={i+1}>{i+1}</option>
+                    ))}
+                </select>
+            </div>
             <div className="results">
                 <p><strong>Tip Amount:</strong> ${tip}</p>
                 <p><strong>Total Amount:</strong> ${total}</p>
+                <p><strong>Per Person:</strong> ${perPersonAmount}</p>
             </div>
         </div>
     );
 }
 
 export default App;
-
-
