f12c33e8542d1558dfe5ffa3ed09b394520cda4b
Peter McClelland <peterwmcclelland@gmail.com>
2023-09-22 11:27:23 -0500

    custom tip feature

--- src/App.js
+++ src/App.js
@@ -1,58 +1,105 @@
-import React, { useState } from 'react';
-import './App.scss';
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
-    const [bill, setBill] = useState('');
-    const [tip, setTip] = useState(0);
-    const [total, setTotal] = useState(0);
-    const [splitCount, setSplitCount] = useState(1);
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [splitCount, setSplitCount] = useState(1);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedTip, setSelectedTip] = useState(null);
+  const [customTip, setCustomTip] = useState("");
 
-    const calculateTip = (percentage) => {
-        let calculatedTip = (bill * (percentage / 100)).toFixed(2);
-        let calculatedTotal = (parseFloat(bill) + parseFloat(calculatedTip)).toFixed(2);
-        setTip(calculatedTip);
-        setTotal(calculatedTotal);
-    };
 
-    const perPersonAmount = (total / splitCount).toFixed(2);
+  const calculateTip = (percentage) => {
+    setSelectedTip(percentage);
+    let calculatedTip = (bill * (percentage / 100)).toFixed(2);
+    let calculatedTotal = (
+      parseFloat(bill) + parseFloat(calculatedTip)
+    ).toFixed(2);
+    setTip(calculatedTip);
+    setTotal(calculatedTotal);
+  };
 
-    return (
-        <div className="app">
-            <h1>Here's A Tip</h1>
-            <div className="input-group">
-                <label>Bill Amount:</label>
-                <input
-                    type="number"
-                    value={bill}
-                    onChange={(e) => setBill(e.target.value)}
-                />
-            </div>
-            <div className="input-group">
-                <label>Select Tip Percentage:</label>
-                <div className="percentage-buttons">
-                    <button onClick={() => calculateTip(10)}>10%</button>
-                    <button onClick={() => calculateTip(15)}>15%</button>
-                    <button onClick={() => calculateTip(20)}>20%</button>
+  const perPersonAmount = (total / splitCount).toFixed(2);
 
-                   
-                </div>
-                <div id='splitbill' className="input-group">
-                <label>Split Bill:</label>
-                <select value={splitCount} onChange={(e) => setSplitCount(e.target.value)}>
-                    {Array.from({length: 10}, (_, i) => (
-                        <option key={i+1} value={i+1}>{i+1}</option>
-                    ))}
-                </select>
-            </div>
-            </div>
-            
-            <div className="results">
-                <p><strong>Tip Amount:</strong> ${tip}</p>
-                <p><strong>Total Amount:</strong> ${total}</p>
-                <p><strong>Per Person:</strong> ${perPersonAmount}</p>
-            </div>
+  return (
+    <div className="app">
+      <h1>Here's A Tip</h1>
+      <div className="input-group">
+        <label>Bill Amount:</label>
+        
+        <input
+          type="number"
+          value={bill}
+          onChange={(e) => setBill(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label>Select Tip Percentage:</label>
+        <div className="tip-selection">
+        <div className="percentage-buttons">
+    <input
+    id="custom-tip-input"
+      value={customTip}
+      onChange={(e) => {
+        setCustomTip(e.target.value);
+        calculateTip(parseFloat(e.target.value));
+      }}
+      placeholder="Custom"
+      className="custom-tip-input"
+    />
+        
+          <button
+            className={selectedTip === 10 ? "active" : ""}
+            onClick={() => calculateTip(10)}
+          >
+            10%
+          </button>
+          <button
+            className={selectedTip === 15 ? "active" : ""}
+            onClick={() => calculateTip(15)}
+          >
+            15%
+          </button>
+          <button
+            className={selectedTip === 20 ? "active" : ""}
+            onClick={() => calculateTip(20)}
+          >
+            20%
+          </button>
         </div>
-    );
+      </div>
+      <div id="splitbill" className={`input-group ${showDropdown ? 'showDropdown' : ''}`}>
+    <div onClick={() => setShowDropdown(!showDropdown)}>Split Bill:</div>
+    <select
+        className="num-people"
+        value={splitCount}
+        onChange={(e) => setSplitCount(e.target.value)}
+    >
+        {Array.from({ length: 10 }, (_, i) => (
+            <option key={i + 1} value={i + 1}>
+                {i + 1}
+            </option>
+        ))}
+    </select>
+</div>
+      <div className="results">
+        <p>
+          <strong>Tip Amount:</strong> ${tip}
+        </p>
+        <p>
+          <strong>Total Amount:</strong> ${total}
+        </p>
+        {showDropdown && (
+          <p>
+            <strong>Per Person:</strong> ${perPersonAmount}
+          </p>
+        )}
+      </div>
+    </div>
+    </div>
+  );
 }
 
 export default App;
